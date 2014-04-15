1ed0da6f11f66add6af1bd057a726982549d94b4
Ashutosh Dhundhara <ashutoshdhundhara@yahoo.com>
2014-04-15 23:19:00 +0530

    Initialized Mail constants.
    Signed-off-by: Ashutosh Dhundhara <ashutoshdhundhara@yahoo.com>

---
+++ libraries/mailConstants.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Contains constants required for sending emails.
+ */
+
+// Define SMTP server.
+define('smtpServer', '');
+// Define SMTP port.
+define('smtpPort', '');
+// Define SMTP Username.
+define('smtpUsername', '');
+// Define SMTP Password.
+define('smtpPassword', '');
+// Define 'From' email address.
+define('smtpFromEmail', '');
+// Define 'From' Name.
+define('smtpFromName', '');
+
+?>
