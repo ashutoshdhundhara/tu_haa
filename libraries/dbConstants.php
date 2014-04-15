5153a1529e9d2bb442e744bbffa5f53baaa6f1cc
Ashutosh Dhundhara <ashutoshdhundhara@yahoo.com>
2014-04-15 23:15:16 +0530

    Redefined DB constants and added table names.
    Signed-off-by: Ashutosh Dhundhara <ashutoshdhundhara@yahoo.com>

--- libraries/dbConstants.php
+++ libraries/dbConstants.php
@@ -3,13 +3,32 @@
  * Contains constants required for DB interactions.
  */
 
+/**
+ * Define global constants for database connection.
+ */
 // Define DB server.
-define('dbHost', '');
+define('dbHost', 'localhost');
 // Define DB Username.
-define('dbUser', '');
+define('dbUser', 'root');
 // Define DB Password.
-define('dbPass', '');
+define('dbPass', 'logMein');
 // Define DB Name.
-define('dbName', '');
+define('dbName', 'HostelJ');
+
+/**
+ * Define constants for databse tables.
+ */
+//Define `student_details` table.
+define('tblStudent', 'student_details');
+//Define `group_details` table.
+define('tblGroup', 'group_details');
+//Define `group_id` table.
+define('tblGroupId', 'group_id');
+// Define `eligible_students` table.
+define('tblEligibleStudents', 'eligible_students');
+// Define `room_info` table.
+define('tblRoom', 'room_info');
+// Define `admin_id` table.
+define('tblAdmin', 'admin_id');
 
 ?>
