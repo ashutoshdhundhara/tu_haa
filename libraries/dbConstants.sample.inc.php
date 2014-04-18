b90dea13483e9216bb61f67d587de98ae9ccd259
Ashutosh Dhundhara <ashutoshdhundhara@yahoo.com>
2014-04-18 18:34:29 +0530

    Created sample constants files.
    Signed-off-by: Ashutosh Dhundhara <ashutoshdhundhara@yahoo.com>

---
+++ libraries/dbConstants.sample.inc.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Contains constants required for DB interactions.
+ */
+
+/**
+ * Define global constants for database connection.
+ */
+// Define DB server.
+define('dbHost', '');
+// Define DB Username.
+define('dbUser', '');
+// Define DB Password.
+define('dbPass', '');
+// Define DB Name.
+define('dbName', '');
+
+/**
+ * Define constants for databse tables.
+ */
+//Define table for storing Student details.
+define('tblStudent', '');
+//Define table for storing Group details.
+define('tblGroup', '');
+//Define table for storing Group credentials.
+define('tblGroupId', '');
+// Define table for storing Eligible students.
+define('tblEligibleStudents', '');
+// Define table for storing Rooms details.
+define('tblRoom', '');
+// Define table for Admin credentials.
+define('tblAdmin', '');
+
+?>
