97c0538cd918a4b1728c0dc33d5600d510d60de3
Ashutosh Dhundhara <ashutoshdhundhara@yahoo.com>
2014-05-13 02:39:39 +0530

    Include indexes in CREATE queries.
    Signed-off-by: Ashutosh Dhundhara <ashutoshdhundhara@yahoo.com>

--- setup/HostelJ.sql
+++ setup/HostelJ.sql
@@ -14,7 +14,8 @@ SET time_zone = "+05:30";
 CREATE TABLE IF NOT EXISTS `admin_id` (
   `admin_id` varchar(10) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `allotment_status` varchar(20) NOT NULL DEFAULT 'DISABLED'
+  `allotment_status` varchar(20) NOT NULL DEFAULT 'DISABLED',
+  PRIMARY KEY (`admin_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Contains Admin credentials.';
 
 -- --------------------------------------------------------
@@ -23,9 +24,10 @@ CREATE TABLE IF NOT EXISTS `admin_id` (
 -- Table structure for table `eligible_students`
 --
 
-CREATE TABLE IF NOT EXISTS `eligible_students` (
+CREATE TABLE IF NOT EXISTS  `eligible_students` (
   `roll_no` varchar(12) NOT NULL,
-  `full_name` varchar(50) NOT NULL
+  `full_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`roll_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Contains list of students eligible for Hostel-J.';
 
 -- --------------------------------------------------------
@@ -37,7 +39,8 @@ CREATE TABLE IF NOT EXISTS `eligible_students` (
 CREATE TABLE IF NOT EXISTS `group_details` (
   `roll_no` varchar(12) NOT NULL,
   `unique_id` varchar(10) NOT NULL,
-  `group_id` varchar(10) NOT NULL
+  `group_id` varchar(10) NOT NULL,
+  PRIMARY KEY (`roll_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Contains details about Groups formed.';
 
 -- --------------------------------------------------------
@@ -50,7 +53,8 @@ CREATE TABLE IF NOT EXISTS `group_id` (
   `group_id` varchar(10) NOT NULL,
   `password` char(128) NOT NULL,
   `group_size` varchar(2) NOT NULL,
-  `allotment_status` varchar(20) NOT NULL DEFAULT 'SELECT'
+  `allotment_status` varchar(20) NOT NULL DEFAULT 'NOT_ALLOTED',
+  PRIMARY KEY (`group_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Contains credentials of Groups formed.';
 
 -- --------------------------------------------------------
@@ -65,7 +69,8 @@ CREATE TABLE IF NOT EXISTS `rooms_info` (
   `cluster` char(1) NOT NULL,
   `room_no` varchar(2) NOT NULL,
   `room_status` varchar(20) NOT NULL DEFAULT 'AVAILABLE',
-  `group_id` varchar(10) NOT NULL
+  `group_id` varchar(10) NOT NULL,
+  PRIMARY KEY (`wing`,`floor`,`cluster`,`room_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Contains details about Hostel Rooms.';
 
 -- --------------------------------------------------------
@@ -94,45 +99,6 @@ CREATE TABLE IF NOT EXISTS `student_details` (
   `alternate_address` varchar(200) DEFAULT NULL,
   `landline` varchar(25) DEFAULT NULL,
   `photo` varchar(100) NOT NULL,
-  `room_no` char(6) DEFAULT NULL
+  `room_no` char(6) DEFAULT NULL,
+  PRIMARY KEY (`roll_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Contains Student''s personal details';
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin_id`
---
-ALTER TABLE `admin_id`
- ADD PRIMARY KEY (`admin_id`);
-
---
--- Indexes for table `eligible_students`
---
-ALTER TABLE `eligible_students`
- ADD PRIMARY KEY (`roll_no`);
-
---
--- Indexes for table `group_details`
---
-ALTER TABLE `group_details`
- ADD PRIMARY KEY (`roll_no`), ADD UNIQUE KEY `unique_id` (`unique_id`);
-
---
--- Indexes for table `group_id`
---
-ALTER TABLE `group_id`
- ADD PRIMARY KEY (`group_id`);
-
---
--- Indexes for table `rooms_info`
---
-ALTER TABLE `rooms_info`
- ADD PRIMARY KEY (`wing`,`floor`,`cluster`,`room_no`);
-
---
--- Indexes for table `student_details`
---
-ALTER TABLE `student_details`
- ADD PRIMARY KEY (`roll_no`);
