bcbf2c1b5825c14530ce6efd778d4a2bfcbdcd32
Vidhant Maini <vidhant_14@hotmail.com>
2014-05-23 04:45:21 +0530

    Added the help page.
    Deleted an unnecessary file.
    
    Pages in footer open in new tabs.

---
+++ help.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Help page.
+ */
+
+/**
+ * Get all required libraries.
+ */
+require_once 'libraries/common.inc.php';
+
+$response = HAA_Response::getInstance();
+$header = $response->getHeader();
+$header->disableGlobalMessage();
+$header->addFile('jquery/jquery.tools.min.js', 'js');
+$header->addFile('help.js', 'js');
+$header->addFile('help.css', 'css');
+$header->addFile('simple_overlay.css', 'css');
+$header->setTitle('Help');
+
+$html_output = '<div class="help_content gray_grad box">'
+    . '<h2>Instructions</h2>'
+    . '<h2>Step-1 : Registration :</h2>'
+    . '<ul style="list-style-type: disc;">'
+    . '<li>Go to <a href="register.php" class="blue" target="_blank">Register</a>'
+    . ' page and fill in your Personal details.<br>'
+    . '<strong>NOTE : </strong>You will be able'
+    . ' to register only if you have been allotted Hostel-J on Web Kiosk.'
+    . '</li>'
+    . '<li>For registration, use the '
+    . '<a href="#" class="blue" rel="#simple_overlay" data-file="snapshot_unique_ID.png">Unique ID</a>'
+    . ' you received on Web-Kiosk.</li>'
+    . '<li>Now, you must decide whether you want to take Rooms in a ' 
+    . '<a href="#" class="blue" rel="#simple_overlay" data-file="snapshot_group.png">Group</a>'
+    . ' (of 2-11 students) or '
+    . '<a href="#" class="blue" rel="#simple_overlay" data-file="snapshot_individual.png">Individually</a>.</li>'
+    . '<li>If you choose to take a room <strong>Individually</strong> '
+    . 'then you have to choose a password. In this case, your Unique ID generated on Web-Kiosk '
+    . 'will be used as your Login ID to take rooms.</li>'
+    . '</ul>'
+    . '<h2>Step-2 : Group Formation :</h2>'
+    . '<ul style="list-style-type: disc;">'
+    . '<li>If you have registered to take room <strong>Individually</strong>'
+    . ' in the <strong>Step-1</strong>, then skip this step.</li>'
+    . '<li>After all members have registered, go to '
+    . '<a href="group.php" class="blue" target="_blank">Create Group</a> page.'
+    . '</li>'
+    . '<li>Select the group size and fill out the necessary details.'
+    . ' </li>'
+    . '<li>On successful submission, a group '
+    . '<a href="#" class="blue" rel="#simple_overlay" data-file="snapshot_groupID.png">Login ID</a> '
+    . 'will be created. Use this Login ID to take rooms for the entire group.</li>'
+    . '</ul>'
+    . '<h2>Step-3 : Room Allotment :</h2>'
+    . '<ul style="list-style-type: disc;">'
+    . '<li>Now, when Room Allotment starts, you have to login into the system '
+    . 'using corresponding Login ID.'
+    . '</li>'
+    . '<li>Choose a Cluster from <a href="#" class="blue" rel="#simple_overlay"'
+    . ' data-file="snapshot_wingmap.png">Wing-Map</a>.'
+    . '</li>'
+    . '<li>Choose room(s) from <a href="#" class="blue" rel="#simple_overlay"'
+    . ' data-file="snapshot_clustermap.png">Cluster-Map</a> '
+    . 'and submit choices.'
+    . '</li>'
+    . '<li>You will get the selected rooms based on availability.'
+    . '</li>'
+    . '<li>After getting the rooms, you can '
+    . '<a href="#" class="blue" rel="#simple_overlay" data-file="snapshot_reorder.png">reorder</a> ' 
+    . 'them to assign rooms to members of the group.'  
+    . '</li>'    
+    . '</ul>'
+    . '<strong>NOTE</strong>: In case of any further issues, you can report them on our'
+    . ' <a href="complaint.php" class="blue" target="_blank">Report Issue</a> page.';
+
+// Overlay container.
+$html_output .= '<div class="simple_overlay" id="simple_overlay">'
+    . '<img id="overlay_image" src="" />'
+    . '<div class="details">'
+    . '<h2></h2>'
+    . '<p></p>'
+    . '</div>'
+    . '</div>'
+    . '</div>';
+
+$response->addHTML($html_output);
+$response->response();
+?>
---
+++ js/help.js
@@ -0,0 +1,74 @@
+/**
+ * Handles frontend for help page.
+ */
+
+$(document).ready(function () {
+    $("a[rel]").overlay({
+        onBeforeLoad: function (event) {
+            // Get filename.
+            var filename = $(event.target).attr('data-file');
+            // Append overlay image src.
+            $('#overlay_image')
+            .attr('src', 'img/snapshots/' + filename);
+            // Add caption to image.
+            $('#simple_overlay')
+            .find('h2')
+            .text(messages[filename][0]);
+            // Add image description.
+            $('#simple_overlay')
+            .find('p')
+            .text(messages[filename][1]);
+        },
+        onClose: function () {
+            // Clear image src.
+            $('#overlay_image')
+            .attr('src', '');
+            // Clear caption to image.
+            $('#simple_overlay')
+            .find('h2')
+            .text('');
+            // Clear image description.
+            $('#simple_overlay')
+            .find('p')
+            .text('');
+        }
+    });
+});
+
+/**
+ * Holds all image captions and descriptions.
+ * @type {Array}
+ */
+var messages = Array();
+messages['snapshot_unique_ID.png'] = Array(
+    'Unique ID',
+    'Use the Unique ID you received on Web-Kiosk to register.'
+)
+messages['snapshot_group.png'] = Array(
+    'Group Registration',
+    'Select group option to register as part of a group.'
+)
+messages['snapshot_individual.png'] = Array(
+    'Individual Registration',
+    'Select individual option to register as an individual.'
+)
+messages['snapshot_loginID_individual.png'] = Array(
+    'LoginID',
+    'A system generated LoginID will be issued on successful registration.'
+)
+messages['snapshot_groupID.png'] = Array(
+    'GroupID',
+    'A system generated group LoginID will be issued for the entire group.'
+)
+messages['snapshot_wingmap.png'] = Array(
+    'Wing Map',
+    'Choose the wing and then select the cluster you the want room in.'
+)
+messages['snapshot_clustermap.png'] = Array(
+    'Cluster Map',
+    'Select the rooms you want from your desired cluster.'
+)
+messages['snapshot_reorder.png'] = Array(
+    'Reorder',
+    'Drag and reorder member names to assign rooms within the group.'
+);
